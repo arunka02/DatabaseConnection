53d2fc5a536317840164947cd6f52ec0b4dc11f7
Arun Anand <arunka02@gmail.com>
2021-12-21 20:42:20 -0800

    Initial commit

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","HR","HR");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
